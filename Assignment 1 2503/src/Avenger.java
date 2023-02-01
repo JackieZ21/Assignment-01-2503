986c6efa3b2af9085919d4412a047e851c173a78
Jackie Zheng <j4ckmanz14@gmail.com>
2023-01-31 21:13:51 -0700

    Avenger class changes

--- Assignment 1 2503/src/Avenger.java
+++ Assignment 1 2503/src/Avenger.java
@@ -1,12 +1,59 @@
+public class Avenger implements Comparable<Avenger> {
+	private String alias;
+	private String lastName;
+	private int frequency;
+	private int order;
 
-public class Avenger implements Comparable <Avenger>{
-
-	// TODO: Implement the Avenger Class
-	
-	@Override
-	public int compareTo(Avenger o) {
-		// TODO Auto-generated method stub
-		return 0;
+	public Avenger(String alias, String lastName) {
+		this.alias = alias;
+		this.lastName = lastName;
+		this.frequency = 1;
+		this.order = 0;
 	}
 
+	public String getAlias() {
+		return alias;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public int getFrequency() {
+		return frequency;
+	}
+
+	public int getOrder() {
+		return order;
+	}
+
+	public void setOrder(int order) {
+		this.order = order;
+	}
+
+	public void increaseFrequency() {
+		frequency++;
+	}
+
+	@Override
+	public int compareTo(Avenger o) {
+		if (this.frequency == o.frequency) {
+			if (this.order < o.order) {
+				return -1;
+			} else if (this.order > o.order) {
+				return 1;
+			} else {
+				return 0;
+			}
+		} else if (this.frequency < o.frequency) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return this.alias + " " + this.lastName + " " + this.frequency;
+	}
 }
