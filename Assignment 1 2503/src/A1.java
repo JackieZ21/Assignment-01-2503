cf54dfe1f43955fd373be1443430c2d77bf43d8b
JordanMercurio <jmerc673@mtroyal.ca>
2023-02-06 21:26:07 -0700

    Edited printResult

--- Assignment 1 2503/src/A1.java
+++ Assignment 1 2503/src/A1.java
@@ -56,7 +56,7 @@ public class A1 {
 		
 		*/ 
 		//input = new Scanner(System.in);
-		Scanner input = new Scanner(new File("src/input2.txt"));
+		Scanner input = new Scanner(new File("src/input3.txt"));
 		while(input.hasNext()) {
 		  String word = input.next().trim().toLowerCase().replaceAll("[^a-z]", "");
 		  if (word.length()>0) {
@@ -110,8 +110,8 @@ public class A1 {
 		//Sorts the avengers that are the most popular to least
 		//fixed the sorting for popular
 			Collections.sort(avengersArrayList, (avenger1, avenger2) -> avenger2.getFrequency() - avenger1.getFrequency());
-				for(int i = 0; i < topN && i < avengersArrayList.size(); i++) {
-					System.out.println(avengersArrayList.get(i).getAlias() + " aka " + avengersArrayList.get(i).getLastName() + " mentioned " + avengersArrayList.get(i).getFrequency() + " time(s) ");
+				for(int most = 0; most < topN && most < avengersArrayList.size(); most++) {
+					System.out.println(avengersArrayList.get(most).getAlias() + " aka " + avengersArrayList.get(most).getLastName() + " mentioned " + avengersArrayList.get(most).getFrequency() + " time(s) ");
 		
 				}
 		System.out.println();
@@ -123,8 +123,8 @@ public class A1 {
 		//Sorts avengers that are least popular to most
 		//fix the sorting for least
 			Collections.sort(avengersArrayList, (avenger1, avenger2) -> avenger1.getFrequency() - avenger2.getFrequency());
-				for(int i = 0; i < topN && i < avengersArrayList.size(); i++) {
-					System.out.println(avengersArrayList.get(i).getAlias() + " aka " + avengersArrayList.get(i).getLastName() + " mentioned " + avengersArrayList.get(i).getFrequency() + " time(s) ");
+				for(int least = 0; least < topN && least < avengersArrayList.size(); least++) {
+					System.out.println(avengersArrayList.get(least).getAlias() + " aka " + avengersArrayList.get(least).getLastName() + " mentioned " + avengersArrayList.get(least).getFrequency() + " time(s) ");
 					
 				}
 		System.out.println();
